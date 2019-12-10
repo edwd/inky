f784f3b126f686f9be8c905bb1e991d4721c4121
edwd <edwd@users.noreply.github.com>
2019-12-10 16:00:52 -0500

    Updating dependencies, including cheerio. This requires falling back to cheerio's legacy html parser; default options are updated

--- test/lib/compare.js
+++ test/lib/compare.js
@@ -4,22 +4,12 @@ const htmlEqual = require('assert-html-equal');
 
 /**
  * Takes HTML input, runs it through the Inky parser, and compares the output to what's expected.
- * @param {string} input - HTML input.
- * @param {string} expected - Expected HTML output.
+ * @param {string} input HTML input
+ * @param {string} expected Expected HTML output
+ * @param {object} cheerioOpts Options to pass to the cheerio library
  * @throws {Error} Throws an error if the output is not identical.
  */
 module.exports = function compare(input, expected, cheerioOpts) {
-  const defaultCheerioOptions = {
-    xml: {
-      withDomLvl1: false,
-      normalizeWhitespace: false,
-      xmlMode: false,
-      decodeEntities: false
-    }
-  };
-
-  cheerioOpts = Object.assign(defaultCheerioOptions, cheerioOpts);
-
   const inky = new Inky();
   const output = inky.releaseTheKraken(input, cheerioOpts);
 
