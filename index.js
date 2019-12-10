7245f841ea87f9ece29fda18241509ec31617f10
edwd <edwd@users.noreply.github.com>
2019-12-10 15:14:20 -0500

    Refactoring:  - Updating syntax to ES6 where appropriate, including let/const, template strings, for...of, and removal of the babel plugin for mocha test framework  - Adding many comments and renaming variables; clarifying some conditional statements, including casting to appropriate types before comparing (where appropriate)  - Refactor raw tag handling functions  - Removing unused or removed dependencies

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var cheerio = require('cheerio');
-var path = require('path');
-var through = require('through2');
-var vfs = require('vinyl-fs');
-var Inky = require('./lib/inky');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
+'use strict';
+/**
+ * @module Inky
+ */
+const path = require('path');
+const through = require('through2');
+const vfs = require('vinyl-fs');
+const Inky = require('./lib/inky');
+const fs = require('fs');
 
 var inky;
 
 module.exports = function(opts, cb) {
-  var stream;
+  let stream;
   opts = opts || {};
   opts.cheerio = Inky.mergeCheerioOpts(opts.cheerio);
   if (typeof inky === 'undefined') {
@@ -31,26 +33,31 @@ module.exports = function(opts, cb) {
     return transform();
   }
 
-  // This transform function takes in a Vinyl HTML file, converts the code from Inky to HTML, and returns the modified file.
+  /**
+   * This transform function takes in a Vinyl HTML file, converts the code from Inky to HTML, and returns the modified file via callback.
+   * If a `dest` option was provided, the file will be written to disk.
+   */
   function transform() {
     return through.obj(function(file, enc, callback) {
-      
-      var convertedHtml = inky.releaseTheKraken(file.contents.toString(), opts.cheerio);
+      const convertedHtml = inky.releaseTheKraken(file.contents.toString(), opts.cheerio);
 
-      file.contents = new Buffer(convertedHtml);
+      file.contents = Buffer.from(convertedHtml);
 
-      // Write to disk manually if the user specified it
-      if (opts.dest) {
-        var outputPath = path.join(opts.dest, path.basename(file.path));
-        mkdirp(opts.dest, function() {
-          fs.writeFile(outputPath, convertedHtml, callback);
+      if (typeof opts.dest === 'string') {
+        // Write to disk manually if the user specified it by providing a file path in the `dest` key of the options
+        const outputPath = path.join(opts.dest, path.basename(file.path));
+        fs.mkdir(opts.dest, { recursive: true }, function(err) {
+          if (!err) {
+            fs.writeFile(outputPath, convertedHtml, callback);
+          } else {
+            callback(err, null);
+          }
         });
-      }
-      else {
+      } else {
         callback(null, file);
       }
     });
   }
-}
+};
 
 module.exports.Inky = Inky;
